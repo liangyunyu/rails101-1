63668eebc9160a6b143211c0f26d5c97b10f58d2
lyy <liangyunyu@gmail.com>
2017-02-23 15:45:24 +0800

    create groups index

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,7 @@
 class WelcomeController < ApplicationController
+  def index
+    flash[:notice] = "Notice：早安！您好！"
+    flash[:alert] = "Alert：晚安！该睡了！"
+    flash[:warning] = "Warning：这是 warning 讯息！"
+  end
 end
